5da64bb4ccde3049a9c6a1265c0c6d66bc3a6c1d
loryruta <loryruta23@gmail.com>
2017-03-05 20:04:15 +0100

    Create asset system

--- opencraft-client/src/main/java/xyz/upperlevel/opencraft/client/asset/AssetManager.java
+++ opencraft-client/src/main/java/xyz/upperlevel/opencraft/client/asset/AssetManager.java
@@ -1,4 +1,17 @@
 package xyz.upperlevel.opencraft.client.asset;
 
+import lombok.Getter;
+import xyz.upperlevel.opencraft.client.asset.shape.BlockShapeManager;
+import xyz.upperlevel.opencraft.client.asset.texture.TextureManager;
+
 public class AssetManager {
+
+    @Getter
+    private BlockShapeManager shapeManager = new BlockShapeManager();
+
+    @Getter
+    private TextureManager textureManager  = new TextureManager();
+
+    public AssetManager() {
+    }
 }
