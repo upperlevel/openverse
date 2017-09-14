ae15d63c4248cb96e0a9567201a0850d6beb6a11
Rossi Lorenzo <snowycoder@gmail.com>
2017-09-14 11:32:18 +0200

    Add run scripts

---
+++ openverse-launcher/src/main/dist/start.bat
@@ -0,0 +1,6 @@
+java -Dcom.sun.management.jmxremote.port=9005 ^
+     -Dcom.sun.management.jmxremote.authenticate=false ^
+     -Dcom.sun.management.jmxremote.ssl=false  ^
+     -Xverify:none ^
+     -XX:+DoEscapeAnalysis ^
+     -jar launcher.jar
---
+++ openverse-launcher/src/main/dist/start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+java -Dcom.sun.management.jmxremote.port=9005 \
+     -Dcom.sun.management.jmxremote.authenticate=false \
+     -Dcom.sun.management.jmxremote.ssl=false  \
+     -Xverify:none \
+     -XX:+DoEscapeAnalysis \
+     -jar launcher.jar
